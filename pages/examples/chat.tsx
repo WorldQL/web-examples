383f25bb10a510b92f4ca579209d279c682b0f71
Jack Baron <jackmbaron@gmail.com>
2021-11-03 18:42:03 +0000

    add basic pages

---
+++ pages/examples/chat.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const ChatExample: NextPage = () => (
+  <>
+    <Head>
+      <title>WorldQL Examples | Chat</title>
+    </Head>
+
+    <div>
+      <h1>Chat Example</h1>
+      <p>Realtime chat room implemented using WorldQL</p>
+    </div>
+  </>
+)
+
+export default ChatExample
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,24 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
 
-const Home: NextPage = () => null
+const Home: NextPage = () => (
+  <>
+    <Head>
+      <title>WorldQL Examples</title>
+    </Head>
+
+    <div>
+      <h1>WorldQL Examples</h1>
+      <sub>Web examples using TypeScript.</sub>
+
+      <hr />
+
+      <ul>
+        <Link href='/examples/chat'>Chat</Link>
+      </ul>
+    </div>
+  </>
+)
 
 export default Home
