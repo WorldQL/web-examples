ce2e5d07166d72a7fa9c4f409309e9814946e095
Jack Baron <jackmbaron@gmail.com>
2021-11-15 09:47:42 +0000

    update worldql hook to use new client fns

--- lib/hooks/useWorldQL.ts
+++ lib/hooks/useWorldQL.ts
@@ -1,10 +1,17 @@
-import { Client as WorldQLClient } from '@worldql/client'
-import type { IncomingMessage } from '@worldql/client'
+import { ClientEvents, Client as WorldQLClient } from '@worldql/client'
 import { useCallback, useEffect, useRef, useState } from 'react'
 
-export type OnMessage = (message: IncomingMessage) => void
+export type Handler<T extends keyof ClientEvents> = (
+  ...args: ClientEvents[T]
+) => void
 
-export const useWorldQL = (url: string, onMessage?: OnMessage) => {
+interface Handlers {
+  peerConnect?: Handler<'peerConnect'>
+  peerDisconnect?: Handler<'peerDisconnect'>
+  globalMessage?: Handler<'globalMessage'>
+}
+
+export const useWorldQL = (url: string, handlers?: Handlers) => {
   const clientRef = useRef<WorldQLClient | null>(null)
   const [ready, setReady] = useState<boolean>(false)
 
@@ -26,33 +33,60 @@ export const useWorldQL = (url: string, onMessage?: OnMessage) => {
     setReady(false)
   }, [])
 
-  const handleMessage = useCallback<OnMessage>(
-    message => {
-      if (typeof onMessage === 'function') onMessage(message)
+  const handlePeerConnect = useCallback<Handler<'peerConnect'>>(
+    (...args) => {
+      if (typeof handlers?.peerConnect === 'function')
+        handlers.peerConnect(...args)
     },
-    [onMessage]
+    [handlers]
+  )
+
+  const handlePeerDisconnect = useCallback<Handler<'peerDisconnect'>>(
+    (...args) => {
+      if (typeof handlers?.peerDisconnect === 'function')
+        handlers.peerDisconnect(...args)
+    },
+    [handlers]
+  )
+
+  const handleGlobalMessage = useCallback<Handler<'globalMessage'>>(
+    (...args) => {
+      if (typeof handlers?.globalMessage === 'function')
+        handlers.globalMessage(...args)
+    },
+    [handlers]
   )
 
   useEffect(() => {
     clientRef.current?.addListener('ready', onReady)
     clientRef.current?.addListener('disconnect', onDisconnect)
-    clientRef.current?.addListener('message', handleMessage)
+    clientRef.current?.addListener('peerConnect', handlePeerConnect)
+    clientRef.current?.addListener('peerDisconnect', handlePeerDisconnect)
+    clientRef.current?.addListener('globalMessage', handleGlobalMessage)
 
     return () => {
       clientRef.current?.removeListener('ready', onReady)
       clientRef.current?.removeListener('disconnect', onDisconnect)
-      clientRef.current?.removeListener('message', handleMessage)
+      clientRef.current?.removeListener('peerConnect', handlePeerConnect)
+      clientRef.current?.removeListener('peerDisconnect', handlePeerDisconnect)
+      clientRef.current?.removeListener('globalMessage', handleGlobalMessage)
     }
-  }, [onReady, onDisconnect, handleMessage])
+  }, [
+    onReady,
+    onDisconnect,
+    handlePeerConnect,
+    handlePeerDisconnect,
+    handleGlobalMessage,
+  ])
 
-  type SendMessage = InstanceType<typeof WorldQLClient>['sendMessage']
-  const sendMessage = useCallback<SendMessage>(message => {
+  type SendGlobalMessage = InstanceType<typeof WorldQLClient>['globalMessage']
+  const globalMessage = useCallback<SendGlobalMessage>((...args) => {
     if (clientRef.current === null) {
       throw new Error('client is null')
     }
 
-    clientRef.current.sendMessage(message)
+    clientRef.current.globalMessage(...args)
   }, [])
 
-  return { ready, sendMessage }
+  return { ready, globalMessage }
 }
